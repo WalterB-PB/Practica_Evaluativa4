7e78c79d54204488d0ec353927a4ab8da3c85f77
Snug <Walterandres261@gmail.com>
2025-04-08 17:39:01 -0500

    Clase donde se le asigna un valor a las preguntas de este nivel con su respectivo puntaje y tiempo

--- PreguntaDificil.cs
+++ PreguntaDificil.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_Evaluativa4
+{
+    public class PreguntaDificil : Preguntas
+    {
+        public override int ObtenerPuntaje()
+        {
+            return 30;
+        }
+        public override int ObtenerTiempoLimite()
+        {
+            return 15;
+        }
+    }
+}
