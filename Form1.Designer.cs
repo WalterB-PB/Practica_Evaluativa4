c051390ca184140283b5954555c30b408c078d02
Snug <Walterandres261@gmail.com>
2025-04-08 17:33:31 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace Practica_Evaluativa4
+{
+    partial class frmTrivia
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cmbCategoria = new System.Windows.Forms.ComboBox();
+            this.cmbNivel = new System.Windows.Forms.ComboBox();
+            this.lstHistorial = new System.Windows.Forms.ListBox();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.btnResponder = new System.Windows.Forms.Button();
+            this.btnReiniciar = new System.Windows.Forms.Button();
+            this.rbOpcion1 = new System.Windows.Forms.RadioButton();
+            this.rbOpcion2 = new System.Windows.Forms.RadioButton();
+            this.rbOpcion3 = new System.Windows.Forms.RadioButton();
+            this.rbOpcion4 = new System.Windows.Forms.RadioButton();
+            this.timerPregunta = new System.Windows.Forms.Timer(this.components);
+            this.lblTiempo = new System.Windows.Forms.Label();
+            this.lblPuntaje = new System.Windows.Forms.Label();
+            this.lblPregunta = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblCategoria = new System.Windows.Forms.Label();
+            this.lblNivel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbCategoria
+            // 
+            this.cmbCategoria.FormattingEnabled = true;
+            this.cmbCategoria.Items.AddRange(new object[] {
+            "Ciencia",
+            "Historia",
+            "Cultura General"});
+            this.cmbCategoria.Location = new System.Drawing.Point(58, 73);
+            this.cmbCategoria.Name = "cmbCategoria";
+            this.cmbCategoria.Size = new System.Drawing.Size(121, 21);
+            this.cmbCategoria.TabIndex = 0;
+            // 
+            // cmbNivel
+            // 
+            this.cmbNivel.FormattingEnabled = true;
+            this.cmbNivel.Items.AddRange(new object[] {
+            "Fácil",
+            "Medio",
+            "Difícil"});
+            this.cmbNivel.Location = new System.Drawing.Point(227, 73);
+            this.cmbNivel.Name = "cmbNivel";
+            this.cmbNivel.Size = new System.Drawing.Size(121, 21);
+            this.cmbNivel.TabIndex = 1;
+            // 
+            // lstHistorial
+            // 
+            this.lstHistorial.FormattingEnabled = true;
+            this.lstHistorial.Location = new System.Drawing.Point(615, 125);
+            this.lstHistorial.Name = "lstHistorial";
+            this.lstHistorial.Size = new System.Drawing.Size(120, 95);
+            this.lstHistorial.TabIndex = 2;
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Location = new System.Drawing.Point(103, 180);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(75, 23);
+            this.btnIniciar.TabIndex = 3;
+            this.btnIniciar.Text = "Iniciar";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // btnResponder
+            // 
+            this.btnResponder.Location = new System.Drawing.Point(205, 180);
+            this.btnResponder.Name = "btnResponder";
+            this.btnResponder.Size = new System.Drawing.Size(75, 23);
+            this.btnResponder.TabIndex = 4;
+            this.btnResponder.Text = "Confirmar";
+            this.btnResponder.UseVisualStyleBackColor = true;
+            this.btnResponder.Click += new System.EventHandler(this.btnResponder_Click);
+            // 
+            // btnReiniciar
+            // 
+            this.btnReiniciar.Location = new System.Drawing.Point(312, 180);
+            this.btnReiniciar.Name = "btnReiniciar";
+            this.btnReiniciar.Size = new System.Drawing.Size(75, 23);
+            this.btnReiniciar.TabIndex = 5;
+            this.btnReiniciar.Text = "Reiniciar";
+            this.btnReiniciar.UseVisualStyleBackColor = true;
+            this.btnReiniciar.Click += new System.EventHandler(this.btnReiniciar_Click);
+            // 
+            // rbOpcion1
+            // 
+            this.rbOpcion1.AutoSize = true;
+            this.rbOpcion1.Location = new System.Drawing.Point(429, 134);
+            this.rbOpcion1.Name = "rbOpcion1";
+            this.rbOpcion1.Size = new System.Drawing.Size(68, 17);
+            this.rbOpcion1.TabIndex = 6;
+            this.rbOpcion1.TabStop = true;
+            this.rbOpcion1.Text = "Opcion 1";
+            this.rbOpcion1.UseVisualStyleBackColor = true;
+            // 
+            // rbOpcion2
+            // 
+            this.rbOpcion2.AutoSize = true;
+            this.rbOpcion2.Location = new System.Drawing.Point(429, 168);
+            this.rbOpcion2.Name = "rbOpcion2";
+            this.rbOpcion2.Size = new System.Drawing.Size(68, 17);
+            this.rbOpcion2.TabIndex = 7;
+            this.rbOpcion2.TabStop = true;
+            this.rbOpcion2.Text = "Opcion 2";
+            this.rbOpcion2.UseVisualStyleBackColor = true;
+            // 
+            // rbOpcion3
+            // 
+            this.rbOpcion3.AutoSize = true;
+            this.rbOpcion3.Location = new System.Drawing.Point(429, 203);
+            this.rbOpcion3.Name = "rbOpcion3";
+            this.rbOpcion3.Size = new System.Drawing.Size(68, 17);
+            this.rbOpcion3.TabIndex = 8;
+            this.rbOpcion3.TabStop = true;
+            this.rbOpcion3.Text = "Opcion 3";
+            this.rbOpcion3.UseVisualStyleBackColor = true;
+            // 
+            // rbOpcion4
+            // 
+            this.rbOpcion4.AutoSize = true;
+            this.rbOpcion4.Location = new System.Drawing.Point(429, 230);
+            this.rbOpcion4.Name = "rbOpcion4";
+            this.rbOpcion4.Size = new System.Drawing.Size(68, 17);
+            this.rbOpcion4.TabIndex = 9;
+            this.rbOpcion4.TabStop = true;
+            this.rbOpcion4.Text = "Opcion 4";
+            this.rbOpcion4.UseVisualStyleBackColor = true;
+            // 
+            // lblTiempo
+            // 
+            this.lblTiempo.AutoSize = true;
+            this.lblTiempo.Location = new System.Drawing.Point(408, 86);
+            this.lblTiempo.Name = "lblTiempo";
+            this.lblTiempo.Size = new System.Drawing.Size(117, 13);
+            this.lblTiempo.TabIndex = 11;
+            this.lblTiempo.Text = "Mostrar tiempo restante";
+            // 
+            // lblPuntaje
+            // 
+            this.lblPuntaje.AutoSize = true;
+            this.lblPuntaje.Location = new System.Drawing.Point(408, 109);
+            this.lblPuntaje.Name = "lblPuntaje";
+            this.lblPuntaje.Size = new System.Drawing.Size(43, 13);
+            this.lblPuntaje.TabIndex = 12;
+            this.lblPuntaje.Text = "Puntaje";
+            // 
+            // lblPregunta
+            // 
+            this.lblPregunta.AutoSize = true;
+            this.lblPregunta.Location = new System.Drawing.Point(409, 51);
+            this.lblPregunta.Name = "lblPregunta";
+            this.lblPregunta.Size = new System.Drawing.Size(88, 13);
+            this.lblPregunta.TabIndex = 13;
+            this.lblPregunta.Text = "Mostrar Pregunta";
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(317, 10);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(33, 13);
+            this.lblTitulo.TabIndex = 14;
+            this.lblTitulo.Text = "Trivia";
+            // 
+            // lblCategoria
+            // 
+            this.lblCategoria.AutoSize = true;
+            this.lblCategoria.Location = new System.Drawing.Point(55, 57);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(52, 13);
+            this.lblCategoria.TabIndex = 15;
+            this.lblCategoria.Text = "Categoria";
+            // 
+            // lblNivel
+            // 
+            this.lblNivel.AutoSize = true;
+            this.lblNivel.Location = new System.Drawing.Point(224, 57);
+            this.lblNivel.Name = "lblNivel";
+            this.lblNivel.Size = new System.Drawing.Size(31, 13);
+            this.lblNivel.TabIndex = 16;
+            this.lblNivel.Text = "Nivel";
+            // 
+            // frmTrivia
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblNivel);
+            this.Controls.Add(this.lblCategoria);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.lblPregunta);
+            this.Controls.Add(this.lblPuntaje);
+            this.Controls.Add(this.lblTiempo);
+            this.Controls.Add(this.rbOpcion4);
+            this.Controls.Add(this.rbOpcion3);
+            this.Controls.Add(this.rbOpcion2);
+            this.Controls.Add(this.rbOpcion1);
+            this.Controls.Add(this.btnReiniciar);
+            this.Controls.Add(this.btnResponder);
+            this.Controls.Add(this.btnIniciar);
+            this.Controls.Add(this.lstHistorial);
+            this.Controls.Add(this.cmbNivel);
+            this.Controls.Add(this.cmbCategoria);
+            this.Name = "frmTrivia";
+            this.Text = "frmTrivia";
+            this.Load += new System.EventHandler(this.frmTrivia_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbCategoria;
+        private System.Windows.Forms.ComboBox cmbNivel;
+        private System.Windows.Forms.ListBox lstHistorial;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.Button btnResponder;
+        private System.Windows.Forms.Button btnReiniciar;
+        private System.Windows.Forms.RadioButton rbOpcion1;
+        private System.Windows.Forms.RadioButton rbOpcion2;
+        private System.Windows.Forms.RadioButton rbOpcion3;
+        private System.Windows.Forms.RadioButton rbOpcion4;
+        private System.Windows.Forms.Timer timerPregunta;
+        private System.Windows.Forms.Label lblTiempo;
+        private System.Windows.Forms.Label lblPuntaje;
+        private System.Windows.Forms.Label lblPregunta;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblCategoria;
+        private System.Windows.Forms.Label lblNivel;
+    }
+}
+
