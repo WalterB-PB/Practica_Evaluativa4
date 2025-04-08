5da9bef818798cbad580620317014d9c2b820ddd
Snug <Walterandres261@gmail.com>
2025-04-08 17:37:19 -0500

    Absorbiendo el metodo constructor de la clase preguntas y dandole un valor de tiempo y puntaje

--- PreguntaMedia.cs
+++ PreguntaMedia.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_Evaluativa4
+{
+    public class PreguntaMedia : Preguntas
+    {
+        public override int ObtenerPuntaje()
+        {
+            return 20;
+
+
+        }
+
+        public override int ObtenerTiempoLimite()
+        {
+            return 30;
+        }
+    }
+}
