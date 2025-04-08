4b9d3cd0bdf28385411b0e6187a332287662043c
Snug <Walterandres261@gmail.com>
2025-04-08 17:40:57 -0500

    Metodo constructor que ereda a todas los niveles y categorias del problema

--- Preguntas.cs
+++ Preguntas.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_Evaluativa4
+{
+    public abstract class Preguntas
+    {
+        public string Texto { get; set; }
+        public List<string> Opciones { get; set; }
+        public string RespuestaCorrecta { get; set; }
+        public string Categoria { get; set; }
+        public string Nivel { get; set; }
+        public abstract int ObtenerPuntaje();
+        public abstract int ObtenerTiempoLimite();
+
+        public virtual bool EvaluarRespuesta(string respuesta)
+        {
+            return respuesta.Trim().Equals(RespuestaCorrecta.Trim(), StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
