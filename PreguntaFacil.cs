1503966e19f9079912437283493151afcab2fefe
Snug <Walterandres261@gmail.com>
2025-04-08 17:39:26 -0500

    Clase donde se le asigna un valor a las preguntas de este nivel con su respectivo puntaje y tiempo

--- PreguntaFacil.cs
+++ PreguntaFacil.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_Evaluativa4
+{
+    public class PreguntaFacil : Preguntas
+    {
+        public override int ObtenerPuntaje()
+        {
+            return 10;
+        }
+
+        public override int ObtenerTiempoLimite()
+        {
+            return 60;
+        }
+    }
+}
